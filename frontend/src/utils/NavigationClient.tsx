85973f58ed425dc3f401d6cbf2be90bf0de73d19
doublegrey <sergejs.kropinov@incrowdsports.com>
2022-11-21 13:48:01 +0200

    use CustomNavigationClient for msal support

--- frontend/src/utils/NavigationClient.tsx
+++ frontend/src/utils/NavigationClient.tsx
@@ -1,9 +1,6 @@
 import { NavigationClient, NavigationOptions } from "@azure/msal-browser";
 import { History } from "history";
 
-/**
- * This is an example for overriding the default function MSAL uses to navigate to other urls in your webpage
- */
 export class CustomNavigationClient extends NavigationClient {
   private history: History;
 
@@ -12,12 +9,6 @@ export class CustomNavigationClient extends NavigationClient {
     this.history = history;
   }
 
-  /**
-   * Navigates to other pages within the same web application
-   * You can use the useHistory hook provided by react-router-dom to take advantage of client-side routing
-   * @param url
-   * @param options
-   */
   async navigateInternal(url: string, options: NavigationOptions) {
     const relativePath = url.replace(window.location.origin, "");
     if (options.noHistory) {
