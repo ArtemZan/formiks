361230916e775178b30d6cfb793834da337ceeb2
Sergejs Podolaks <sergejspodolaks@Sergejss-MacBook-Pro-2.local>
2023-10-20 09:14:25 +0300

    F

--- backend/middlewares/msal/verify.go
+++ backend/middlewares/msal/verify.go
@@ -86,7 +86,7 @@ func SetRoles() gin.HandlerFunc {
 				roles = []string{"guest"}
 			} else {
 				roles = append(roles, "Administrator")
-				c.AbortWithStatus(http.StatusForbidden)
+				// c.AbortWithStatus(http.StatusForbidden)
 				fmt.Println("Unauthorized")
 				return
 			}
